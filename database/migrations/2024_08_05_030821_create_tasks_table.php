b01fcf972e3a8d695772a04379ca31c409172e0e
aoba <writer_fairy_road@yahoo.co.jp>
2024-08-05 12:15:36 +0900

    🗃️ feat : Add tasks table to store task information.

---
+++ database/migrations/2024_08_05_030821_create_tasks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->longText('description')->nullable();
+            $table->timestamp('due_date');
+            $table->enum('status', ['pending', 'is_progress', 'completed']);
+            $table->enum('priority', ['low', 'medium', 'high']);
+            $table->string('image_path')->nullable();
+            $table->foreignId('created_by')->constrained('users');
+            $table->foreignId('updated_by')->constrained('users');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
