b5829d43970d5e284d402f278dbefececb5fb70f
aoba <writer_fairy_road@yahoo.co.jp>
2024-08-15 12:56:10 +0900

    🦺 Add validation when invite member to project.

---
+++ app/Http/Requests/InviteMemberRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class InviteMemberRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'user_id' => 'required|integer|exists:users,id',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'user_id.required' => 'user_idは必須項目です',
+            'user_id.integer' => 'user_idは数値型のみ有効です',
+            'user_id.exists' => 'userが存在しません'
+        ];
+    }
+}
