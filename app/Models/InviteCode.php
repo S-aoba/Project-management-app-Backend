97fc29f9eec59de868f25efa96f47add5b78738d
S-aoba <writer_fairy_road@yahoo.co.jp>
2024-09-24 21:16:28 +0900

    🗃️ feat: change primary key name, keyType, and invrementing propaty. (#50)

--- app/Models/InviteCode.php
+++ app/Models/InviteCode.php
@@ -11,6 +11,10 @@ class InviteCode extends Model
 {
     use HasFactory;
 
+    protected $primaryKey = 'code';
+    protected $keyType = 'string';
+    public $incrementing = false;
+
     // テーブルカラムにupdated_atはないので、
     // Elonquent上でのupdated_atのデータの挿入をなしにする設定
     const UPDATED_AT = null;
