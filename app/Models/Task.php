84094767c0935f55715988d5b39bbca5e6d9ae24
S-aoba <writer_fairy_road@yahoo.co.jp>
2024-09-29 16:54:44 +0900

    rm create task method.

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,12 +2,8 @@
 
 namespace App\Models;
 
-use App\Http\Requests\StoreTaskRequest;
-use Exception;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Facades\Auth;
 
 class Task extends Model
 {
@@ -25,49 +21,4 @@ class Task extends Model
         'created_by',
         'updated_by'
     ];
-
-
-    static function storeTask(StoreTaskRequest $validatedData): void
-    {
-        // project_idを使ってProjectが存在しているかどうかを判定
-        $projectId = $validatedData['projectId'];
-        
-        if(!Project::find($projectId)) {
-            throw new ModelNotFoundException('Project is not exist.');
-        }
-
-        // assigned_user_idとcreated_byが異なる場合のみユーザーの検証を行う
-        if($validatedData['assignedUserId'] !== $validatedData['created_by'])
-        {
-            // assign_user_idを使い、そのユーザーが存在しているかつ、タスクを作成するProjectへ参加しているかどうかを判定
-            $assignedUserId = $validatedData['assignedUserId'];
-    
-            // ユーザーが存在しているかを確認
-            $user = User::find($assignedUserId);
-            if(is_null($user)){
-                throw new ModelNotFoundException('User is not exist.');
-            }
-
-            // ユーザーがProjectに参加しているかどうかを確認
-            $isJoinProject = $user->projects->where('id', $projectId)->isEmpty();
-            
-            if($isJoinProject) {
-                throw new Exception('No user exist in the specified project.');
-            }            
-        }
-        
-        // 上記の条件をクリアすればDBへ登録する
-        Task::create([
-            'name' => $validatedData['name'],
-            'description' => $validatedData['description'],
-            'due_date' => $validatedData['dueDate'],
-            'status' => $validatedData['status'],
-            'image_path' => $validatedData['imagePath'],
-            'priority' => $validatedData['priority'],
-            'assigned_user_id' => $validatedData['assignedUserId'],
-            'project_id' => $validatedData['projectId'],
-            'created_by' => Auth::id(),
-            'updated_by' => Auth::id()
-        ]);
-    }
 }
