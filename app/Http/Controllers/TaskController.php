43a7effe02125c7cf6277899ab0dc47885e63d4f
S-aoba <writer_fairy_road@yahoo.co.jp>
2024-09-29 16:55:21 +0900

    mv: create task code from task model.

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -6,13 +6,26 @@ use App\Http\Requests\StoreTaskRequest;
 use App\Http\Requests\UpdateTaskRequest;
 use App\Models\Task;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\Auth;
 
 class TaskController extends Controller
 {
     public function store(StoreTaskRequest $request)
     {
         try {
-            Task::storeTask($request);
+            $validatedData = $request->validated();
+            Task::create([
+                'name' => $validatedData['name'],
+                'description' => $validatedData['description'],
+                'due_date' => $validatedData['dueDate'],
+                'status' => $validatedData['status'],
+                'image_path' => $validatedData['imagePath'],
+                'priority' => $validatedData['priority'],
+                'assigned_user_id' => $validatedData['assignedUserId'],
+                'project_id' => $validatedData['projectId'],
+                'created_by' => Auth::id(),
+                'updated_by' => Auth::id()
+            ]);
     
             return response()->json([
                 'status' => true,
