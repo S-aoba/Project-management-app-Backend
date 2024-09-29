43f33a746439c680b5340c0f44933c30aa6d837e
S-aoba <writer_fairy_road@yahoo.co.jp>
2024-09-29 21:42:34 +0900

    fix: task update logic.

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -28,17 +28,14 @@ class TaskController extends Controller
             ]);
     
             return response()->json([
-                'status' => true,
                 'message' => 'Task created Succesfully!'
             ], 201);
         } catch (ModelNotFoundException $e) {
             return response()->json([
-                'status' => false,
                 'message' => $e->getMessage()
             ], 404);
         } catch (\Exception $e) {
             return response()->json([
-                'status' => false,
                 'message' => $e->getMessage()
             ], 500);
         }
@@ -46,10 +43,21 @@ class TaskController extends Controller
 
     public function update(UpdateTaskRequest $request, Task $task)
     {
-        $task->update($request->all());
+        $validatedData = $request->validated();
+        $task->update([
+            'name' => $validatedData['name'],
+            'description' => $validatedData['description'],
+            'due_date' => $validatedData['dueDate'],
+            'status' => $validatedData['status'],
+            'image_path' => $validatedData['imagePath'],
+            'priority' => $validatedData['priority'],
+            'assigned_user_id' => $validatedData['assignedUserId'],
+            'project_id' => $validatedData['projectId'],
+            'created_by' => Auth::id(),
+            'updated_by' => Auth::id()
+        ]);
 
         return response()->json([
-            'status' => true,
             'message' => 'Task updated Successfully!'
         ], 200);
     }
@@ -60,7 +68,6 @@ class TaskController extends Controller
 
         if($res) {
             return response()->json([
-                'status' => true,
                 'message' => 'Task deleted Successfully'
             ], 200);
         }
