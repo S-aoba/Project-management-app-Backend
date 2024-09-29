fc92951da583ba9f78e922963a9d1de9003e8c66
S-aoba <writer_fairy_road@yahoo.co.jp>
2024-09-29 23:51:04 +0900

    add: update assigned user id. (#65)

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ChangeAssignedUserIdRequest;
 use App\Http\Requests\DestroyTaskRequest;
 use App\Http\Requests\StoreTaskRequest;
 use App\Http\Requests\UpdateTaskRequest;
+use App\Models\Project;
 use App\Models\Task;
+use App\Models\User;
 use Exception;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
@@ -31,7 +34,7 @@ class TaskController extends Controller
             ]);
     
             return response()->json([
-                'message' => 'Task created Succesfully!'
+                'message' => 'Task created succesfully!'
             ], 201);
         } catch (ModelNotFoundException $e) {
             return response()->json([
@@ -62,8 +65,42 @@ class TaskController extends Controller
             ]);
     
             return response()->json([
-                'message' => 'Task updated Successfully!'
+                'message' => 'Task updated successfully!'
             ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage()
+            ], $e->getCode());
+        }
+    }
+
+    public function changeAssignedUserId(ChangeAssignedUserIdRequest  $request, Task $task)
+    {
+        try {
+            $newAssignedUserId = $request->validated()['newAssignedUserId'];
+
+            if ($task['assigned_user_id'] === $newAssignedUserId)
+            {
+                throw new Exception('The assigned user is already selected. Please specify a different user.', 403);
+            }
+            
+            $newAssignedUser = User::find($newAssignedUserId);
+    
+            $project = Project::find($task['project_id']);
+            
+            if(!$newAssignedUser->can('checkJoinProject', $project))
+            {
+                throw new Exception('new assigned user is not in project.', 403);
+            }
+    
+           $task->update([
+            'assigned_user_id' => $newAssignedUserId
+           ]);
+
+           return response()->json([
+            'message' => 'Updated assignedUserId successfully!'
+           ],200);
+
         } catch (\Exception $e) {
             return response()->json([
                 'message' => $e->getMessage()
---
+++ app/Http/Requests/ChangeAssignedUserIdRequest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Project;
+use App\Models\User;
+use Illuminate\Foundation\Http\FormRequest;
+
+class ChangeAssignedUserIdRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        $projcetId = $this->route('task')['project_id'];
+        $project = Project::find($projcetId);
+
+        if(!$project)
+        {
+            return false;
+        }
+        
+        $assignedUserId = $this->route('task')['assigned_user_id'];
+        if(!$this->user()->isAdmin($project) && $this->user()->id !== $assignedUserId){
+            return false;
+        }
+        
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'newAssignedUserId' => 'required|int|exists:users,id'
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'newAssignedUserId.required' => 'The new assigned user ID field is required.',
+            'newAssignedUserId.int' => 'The new assigned user ID must be an integer.',
+            'newAssignedUserId.exists' => 'The selected new assigned user does not exist.',
+        ];
+    }
+
+}
